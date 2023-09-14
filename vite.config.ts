727bf7f847aa286e32bc44c0b7ab7d82a9f10dfe
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2023-09-14 16:04:34 -0300

    configurando alias de caminhos e adicionando optimizeDeps para o ffmpeg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+  },
+  resolve: {
+    alias: {
+        "@": path.resolve(__dirname, "./src"),
+    }
+  }
 })
